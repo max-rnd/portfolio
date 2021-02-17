0033ad495d3119928d8ff2f438df245205f5e2b6
Max <60011762+max-rnd@users.noreply.github.com>
2021-02-17 15:21:48 +0100

    Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,14 +1,45 @@
-import React from 'react'
+import React from "react";
 
 class Experience extends React.Component {
   render() {
     return (
       <div class="pt-11 min-h-screen flex flex-col" ref={this.studiesRef}>
         <h2 class="mb-4 text-2xl">Mes expériences professionnel</h2>
-
+        <Exp
+          date="2021"
+          name="FLEURS POIS & CIE"
+          duration="5"
+          description="Création d'un site web de gestion des stocks"
+        />
+        <Exp
+          date="2018 - 2019"
+          name="ECOLE NATIONALEDE SKI ET D’ALPINISME"
+          duration="12"
+          description="Gestion parc informatique –Création de script –Installationd’un reverse proxy –GestionGLPI –Gestion WSUS"
+        />
+        <Exp
+          date="2019"
+          name="COMMUNAUTE DE COMMUNES PAYS DU MONT-BLANC"
+          duration="4"
+          description="Mise en place d’un Firewall -Installation de GLPI"
+        />
       </div>
     );
   }
 }
 
-export default Experience
+export default Experience;
+
+class Exp extends React.Component {
+  render() {
+    return (
+      <div class="shadow-lg p-4 mt-4">
+        <div class="flex justify-between">
+          <h2 class="text-xl">{this.props.name}</h2>
+          <h2 class="text-xl">{this.props.date}</h2>
+        </div>
+        <p>{this.props.duration} semaines de stage</p>
+      </div>
+    );
+  }
+}
