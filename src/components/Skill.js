eae010d55a77ba686b8cbad8cd0cccc5c450a868
Max <60011762+max-rnd@users.noreply.github.com>
2021-02-17 16:48:31 +0100

    skill array test

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React from "react";
 
 class Skill extends React.Component {
   render() {
+    const skills = {
+      Programation: {
+        Langages: ["HTML / CSS", "JS", "PHP", "C#", "SQL"],
+        Framwork: ["Symfony", "Ruby on Rails"],
+        "Bibliothèque JS": ["React", "React Native"],
+        "Framwork CSS": ["Bootstrap", "Tailwind"]
+      },
+      Infrasructure: {
+        "Systèmesd’exploitation": ["Windows", "Mac OS", "Debian"],
+        Hyperviseur: ["Proxmox", "VMware", "AWS EC2", "Virtual Box", "Hyper-V"]
+      }
+    };
     return (
       <div class="pt-11 min-h-screen flex flex-col" ref={this.studiesRef}>
         <h2 class="mb-4 text-2xl">Mes compétances</h2>
-
+        <SkillList skills={skills} />
       </div>
     );
   }
 }
 
-export default Skill
+export default Skill;
+
+class SkillList extends React.Component {
+  constructor(attr) {
+    super(attr);
+  }
+  render() {
+    return (
+      <div class="pt-11 min-h-screen flex flex-col" ref={this.studiesRef}>
+        <h2 class="mb-4 text-2xl">Mes compétances</h2>
+      </div>
+    );
+  }
+}
